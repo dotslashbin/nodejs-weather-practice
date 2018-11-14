c0c65d56eca4da76c9df1a3c5e3823e375181c48
Joshua Raphael Fuentes <joshua.raphael@sorealprop.com>
2018-11-14 16:32:47 +0800

    added implementations

---
+++ app.js
@@ -0,0 +1,33 @@
+const request 	= require('request');
+const yargs 	= require('yargs');
+
+const argv = yargs
+  .options({
+		a: {
+		  demand: true,
+		  alias: 'address',
+		  describe: 'Address to fetch weather for',
+		  string: true
+		}
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
+request({
+	url: `http://www.mapquestapi.com/geocoding/v1/address?key=Yzb1djeq26IvaScB9yefdYHAEOyukanH&location=${encodedAddress}`,
+	json: true
+}, (error, response, body) => {
+		debugger;
+	if (error) {
+		console.log('Unable to connect to Google servers.');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find that address.');
+	} else if (body.status === 'OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
+});
