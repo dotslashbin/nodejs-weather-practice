b764013f50d43069a58a5614166780a0600f9efb
Joshua Raphael Fuentes <joshua.raphael@sorealprop.com>
2018-11-15 13:46:39 +0800

    Modified implementation for proper callback

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request 	= require('request');
 const yargs 	= require('yargs');
+const geocode 	= require('./geocode/geocode.js'); 
 
 const argv = yargs
   .options({
@@ -14,20 +15,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-	url: `http://www.mapquestapi.com/geocoding/v1/address?key=Yzb1djeq26IvaScB9yefdYHAEOyukanH&location=${encodedAddress}`,
-	json: true
-}, (error, response, body) => {
-		debugger;
-	if (error) {
-		console.log('Unable to connect to Google servers.');
-	} else if (body.status === 'ZERO_RESULTS') {
-		console.log('Unable to find that address.');
-	} else if (body.status === 'OK') {
-		console.log(`Address: ${body.results[0].formatted_address}`);
-		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-	}
+geocode.geoCodeAddress(argv.address, (errorMessage, results) => {
+	if (errorMessage) {
+		console.log(errorMessage);
+  	} else {
+    	console.log(JSON.stringify(results, undefined, 2));
+  	}
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,25 @@
+const request 	= require('request');
+
+var geoCodeAddress = (address, callback) => {
+	var encodedAddress = encodeURIComponent(address);
+
+	request({
+		url: `http://www.mapquestapi.com/geocoding/v1/address?key=Yzb1djeq26IvaScB9yefdYHAEOyukanH&location=${encodedAddress}`,
+		json: true
+	}, (error, response, body) => {
+
+		if (error) {
+			callback('Unable to connect to Google servers.');
+		} else if (body.status === 'ZERO_RESULTS') {
+			callback('Unable to find that address.');
+		} else if (body.info.statuscode === 0) {
+			callback(
+				undefined, 
+				body.results[0].locations[0]
+			)
+			// return body.results[0].localStoragecations[0].displayLatLng
+		}
+	});	
+}
+
+module.exports = { geoCodeAddress }
